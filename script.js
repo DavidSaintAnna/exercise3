cdee4cdd2e45e9811647bed1085e0836899198c9
David <david.henri.santana@gmail.com>
2021-11-22 08:32:12 -0300

    exercise

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("brand").src = "logo.png";
+document.getElementById("image").src = "Eu.png";
+let aboutMe = document.getElementById("aboutme");
+aboutMe.innerHTML ="David Santana<br>São Cristovão-SE<br>20 anos";
+let story = document.getElementById("info");
+story.innerHTML ="Meu artista favorito Juice Wrld";
+document.getElementById("image2").src = "juice.jpg";
+let bio = document.getElementById("list");
+bio.innerHTML ="Nome:Jarad Anthony Higgins, Genêro musical : Hip-Hop, Rap, Sad Trap.<br>Jarad Anthony Higgins (Chicago, 2 de dezembro de 1998 — Oak Lawn, 8 de dezembro de 2019),<br> conhecido por seu nome artístico Juice Wrld (estilizado Juice WRLD; pronuncia-se juice world),<br> foi um Vocalista de R&B, rapper, cantor, compositor e músico norte-americano, Era mais conhecido por seus singles de sucesso All Girls Are the Same e Lucid Dreams,<br>que o ajudaram a ganhar um contrato de gravação com a Grade A Productions, de Lil Bibby, e a Interscope Records.<br>" 
+let a = document.getElementById("juicemusic");
+a.href = "https://www.youtube.com/watch?v=rZKQvaLINdY";
+let juicew = document.getElementById("status");
+juicew.innerHTML ="Clique aqui para ver minha playlist preferida do Juice Wrld.";
+document.querySelector(".colour").style.color = "black";
+document.querySelector(".stats").style.color = "blue";
+
